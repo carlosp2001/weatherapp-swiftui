5103c8bdc56f4727045c7d3f4091062a7e863aae
Carlos Pineda <cara_p@unicah.edu>
2023-11-17 14:50:50 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Carlos Pineda on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
